b93e0373d8a969c75e85be4e293d9cf9356ba411
tom-023 <guanlidubian@gmail.com>
2024-10-10 08:30:19 +0900

    :sparkles: dbmanager interface

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,1 +1,11 @@
 package db
+
+import (
+	"database/sql"
+)
+
+// DBManagerインターフェース
+type DBManager interface {
+	Connect(rootUser, rootPassword, host, dbName string) (*sql.DB, error)
+	CreateUser(db *sql.DB, username, password, privileges string) error
+}
